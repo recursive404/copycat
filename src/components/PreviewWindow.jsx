6a726d376e135e0c573ae0ee2450058b05d7854f
recursive <recursive404@gmail.com>
2025-02-14 15:32:43 -0700

    refactor: Update syntax highlighter styles and language imports

--- src/components/PreviewWindow.jsx
+++ src/components/PreviewWindow.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronRight, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vs } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
-import javascript from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
+import javascript from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
 import css from 'react-syntax-highlighter/dist/esm/languages/prism/css';
 
 // Register languages
@@ -43,7 +43,7 @@ const PreviewWindow = ({ files, onRemoveFile }) => {
       <div className="preview-content">
         {files.map((file) => (
           <div key={file.path} className="file-preview-item">
-            <div 
+            <div
               className="file-preview-header"
               onClick={(e) => {
                 // Only trigger if click wasn't on the trash icon
@@ -53,13 +53,13 @@ const PreviewWindow = ({ files, onRemoveFile }) => {
               }}
             >
               <div className="icon-container">
-                <FontAwesomeIcon 
-                  icon={collapsedFiles.has(file.path) ? faChevronRight : faChevronDown} 
+                <FontAwesomeIcon
+                  icon={collapsedFiles.has(file.path) ? faChevronRight : faChevronDown}
                   size="sm"
                 />
               </div>
               <h3 className="file-name" title={file.path}>{file.name}</h3>
-              <div 
+              <div
                 className="remove-icon"
                 onClick={(e) => {
                   e.stopPropagation();
@@ -72,12 +72,12 @@ const PreviewWindow = ({ files, onRemoveFile }) => {
             </div>
             {!collapsedFiles.has(file.path) && (
               <div className="file-preview-content">
-                <SyntaxHighlighter 
+                <SyntaxHighlighter
                   language={getLanguage(file.name)}
-                  style={vs}
+                  style={solarizedlight}
                   showLineNumbers
                   lineNumberStyle={{ color: '#999', marginRight: '1em' }}
-                  customStyle={{ 
+                  customStyle={{
                     backgroundColor: 'transparent',
                     padding: 0,
                     margin: 0
